76126a1b8447dece1bcf9365f73976ddc59a2442
Peter <thong01677@gmail.com>
2021-06-05 21:35:58 -0400

    adding the page change logic

--- js/app.js
+++ js/app.js
@@ -6,29 +6,59 @@ document.addEventListener("scroll", () => {
   const book = document.querySelector(".book");
   const page1 = document.querySelector(".book__page--1");
   const page2 = document.querySelector(".book__page--2");
+  const page4 = document.querySelector(".book__page--4");
+  const page6 = document.querySelector(".book__page--6");
+  const page8 = document.querySelector(".book__page--8");
 
   let percent = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
 
   if (percent > 10) {
-    if (!page1.classList.contains("flip")) {
+    if (!page1.classList.contains("flip1")) {
       book.style.transform = "translateX(0%)";
-      page1.classList.add("flip");
-      page1.onanimationend = () => {
-        page1.style.zIndex = "-1";
-        console.log("end");
-      };
+      page1.classList.add("flip1");
     }
   } else {
-    page1.classList.remove("flip");
-    page1.style.zIndex = "5";
+    page1.classList.remove("flip1");
+    page1.style.zIndex = "10";
     book.style.transform = "translateX(-25%)";
   }
 
   if (percent > 30) {
-    if (!page2.classList.contains("flip")) {
-      page2.classList.add("flip");
+    if (!page2.classList.contains("flip2")) {
+      page2.classList.add("flip2");
+      page1.style.zIndex = "-10";
     }
   } else {
-    page2.classList.remove("flip");
+    page2.classList.remove("flip2");
+    page2.style.zIndex = "9";
+  }
+
+  if (percent > 50) {
+    if (!page4.classList.contains("flip4")) {
+      page4.classList.add("flip4");
+      page2.style.zIndex = "-9";
+    }
+  } else {
+    page4.classList.remove("flip4");
+    page4.style.zIndex = "8";
+  }
+
+  if (percent > 70) {
+    if (!page6.classList.contains("flip6")) {
+      page6.classList.add("flip6");
+      page4.style.zIndex = "-8";
+    }
+  } else {
+    page6.classList.remove("flip6");
+    page6.style.zIndex = "7";
+  }
+
+  if (percent > 90) {
+    if (!page8.classList.contains("flip8")) {
+      page8.classList.add("flip8");
+      page6.style.zIndex = "-7";
+    }
+  } else {
+    page8.classList.remove("flip8");
   }
 });
